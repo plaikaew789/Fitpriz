ad683ca5f47229e4449b2e772be6a3dcccc89c9e
plaikaew789 <pridsadee789@gmail.com>
2023-05-26 20:56:17 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+//preloader
+$(document).ready(function() {
+    $(".preloader-wrapper").fadeOut(1000);
+})
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+
+
